45d751584c62a04ea2a53f7b69583d4be5fc1087
VenciSarov <venci.sarov@gmail.com>
2017-01-26 08:42:26 +0200

    Task - Fold and sum. Problem solved

--- ArraysExercises/03. Fold and Sum/FoldAndSum.cs
+++ ArraysExercises/03. Fold and Sum/FoldAndSum.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _03.Fold_and_Sum
+class FoldAndSum
 {
-    class FoldAndSum
+    static void Main()
     {
-        static void Main(string[] args)
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int len = nums.Length / 2;
+        int[] firstRow = new int[len];
+        int[] secondRow = new int[len];
+        int[] sum = new int[len];
+
+        int index = len / 2 - 1;
+
+        GetFirstRow(index, len, nums, firstRow);
+        GetSecondRow(len, nums, secondRow);
+        GetAndPrintResult(len, sum, firstRow, secondRow);  
+    }
+
+    static void GetAndPrintResult(int len, int[] sum, int[] firstRow, int[] secondRow)
+    {
+        for (int i = 0; i < len; i++)
         {
+            sum[i] = firstRow[i] + secondRow[i];
         }
+
+        Console.WriteLine(string.Join(" ", sum));
+    }
+
+    static int[] GetSecondRow(int len, int[] nums, int[] secondRow)
+    {
+        for (int i = 0; i < len; i++)
+        {
+            secondRow[i] = nums[len / 2 + i];
+        }
+
+        return secondRow;
+    }
+
+    static int[] GetFirstRow(int index, int len, int[] nums, int[] firstRow)
+    {
+        for (int i = 0; i < len; i++)
+        {
+            if (i < len / 2)
+            {
+                firstRow[index] = nums[i];
+                index--;
+            }
+            else if (i >= len / 2)
+            {
+                index++;
+                firstRow[i] = nums[nums.Length - 1 - index];
+            }
+        }
+
+        return firstRow;
     }
 }
+
