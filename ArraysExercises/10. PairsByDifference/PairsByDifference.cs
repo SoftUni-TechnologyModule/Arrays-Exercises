62eaa6ddaeb911a94da13cda5668a6a8889203d4
VenciSarov <venci.sarov@gmail.com>
2017-01-28 15:09:09 +0200

    Problem solved.

--- ArraysExercises/10. PairsByDifference/PairsByDifference.cs
+++ ArraysExercises/10. PairsByDifference/PairsByDifference.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _10.PairsByDifference
+class PairsByDifference
 {
-    class PairsByDifference
+    static void Main()
     {
-        static void Main(string[] args)
+        string inputNums = Console.ReadLine();
+
+        int[] numsArray = inputNums.Split(' ').Select(int.Parse).ToArray();
+        int difference = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(CountPairsByDifference(numsArray, difference));
+    }
+
+    static int CountPairsByDifference(int[] numsArray, int difference)
+    {
+        int arrayLenght = numsArray.Length;
+        int differenceCounter = 0;
+
+        for (int i = 0; i < arrayLenght - 1; i++)
         {
+            for (int r = i + 1; r < arrayLenght; r++)
+            {
+                if (Math.Abs(numsArray[i] - numsArray[r]) == difference)
+                {
+                    differenceCounter++;
+                }
+            }
         }
+
+        return differenceCounter;
     }
 }
+
