ad1a1f5b50046b924caccf20f73b38b092477314
VenciSarov <venci.sarov@gmail.com>
2017-01-28 15:07:51 +0200

    Judge system gives 90/100 points. Test Number 6 gives wrong answer. Code need fixing!

--- ArraysExercises/08. Most Frequent Number/MostFrequentNumber.cs
+++ ArraysExercises/08. Most Frequent Number/MostFrequentNumber.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _08.Most_Frequent_Number
+class MostFrequentNumber
 {
-    class MostFrequentNumber
+    static void Main()
     {
-        static void Main(string[] args)
+        string nums = Console.ReadLine();
+
+        ushort[] numsArray = nums.Split(' ').Select(ushort.Parse).ToArray();
+        ushort len = (ushort)numsArray.Length;
+
+        Console.WriteLine(GetMostFrequentNumber(numsArray, len)); 
+    }
+
+    static int GetMostFrequentNumber(ushort[] numsArray, ushort len)
+    {
+        ushort counter = 1;
+        ushort bestCounter = 0;
+        ushort element = 0;
+            
+        for (int i = 0; i < len-1; i++)
         {
+            for (int r = i + 1; r < len; r++)
+            {
+                if (numsArray[i] == numsArray[r])
+                {
+                    counter++;                  
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (counter > bestCounter)
+            {
+                bestCounter = counter;
+                element = numsArray[i];
+            }
+
+            counter = 1;
         }
+
+        return element;
     }
 }
+
