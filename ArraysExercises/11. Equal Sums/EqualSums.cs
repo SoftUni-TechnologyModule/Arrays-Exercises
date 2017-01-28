5f4e60a03bf7c7fad56aa31371ac67264494c7e3
VenciSarov <venci.sarov@gmail.com>
2017-01-28 18:32:44 +0200

    Code needs fixing! Judge system gives 90/ 100 points. Test number 10 fail!

--- ArraysExercises/11. Equal Sums/EqualSums.cs
+++ ArraysExercises/11. Equal Sums/EqualSums.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _11.Equal_Sums
+class EqualSums
 {
-    class EqualSums
+    static void Main()
     {
-        static void Main(string[] args)
+        long[] numsArray = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+        CheckForEqualSum(numsArray);
+    }
+
+    static void CheckForEqualSum(long[] numsArray)
+    {
+        long leftSum = 0L;
+        long rightSum = 0L;
+        int arrayLength = numsArray.Length;       
+        //bool equalFound = false;
+
+        if (arrayLength == 1)
         {
+            Console.WriteLine("0");
+            return;
         }
+
+        for (int i = 0; i < arrayLength; i++)
+        {
+            leftSum += numsArray[i];
+            
+            for (int r = i + 2; r < arrayLength; r++)
+            {
+                rightSum += numsArray[r];
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine(i + 1);
+                return;
+            }
+
+            rightSum = 0;
+        }
+
+        Console.WriteLine("no");
+        //if (!equalFound)
+        //{
+        //    Console.WriteLine("no");
+        //}
     }
 }
+
