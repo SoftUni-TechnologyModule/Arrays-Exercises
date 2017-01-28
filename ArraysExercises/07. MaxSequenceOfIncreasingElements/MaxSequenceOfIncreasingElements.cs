45d5eb3ef1a65c2319952f0e37ab18c41fd1ec5f
VenciSarov <venci.sarov@gmail.com>
2017-01-28 15:06:08 +0200

    Problem solved.

--- ArraysExercises/07. MaxSequenceOfIncreasingElements/MaxSequenceOfIncreasingElements.cs
+++ ArraysExercises/07. MaxSequenceOfIncreasingElements/MaxSequenceOfIncreasingElements.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _07.MaxSequenceOfIncreasingElements
+class MaxSequenceOfIncreasingElements
 {
-    class MaxSequenceOfIncreasingElements
+    static void Main()
     {
-        static void Main(string[] args)
+        string nums = Console.ReadLine();
+
+        int[] maxSequenceArray = nums.Split(' ').Select(int.Parse).ToArray();
+
+        GetMaxSequenceOfIncreasingElements(maxSequenceArray);
+    }
+
+    static void GetMaxSequenceOfIncreasingElements(int[] nums)
+    {
+        int start = 0;
+        int bestStart = 0;
+        int len = 0;
+        int bestLenght = 0;
+
+        for (int pos = 1; pos < nums.Length; pos++)
         {
+            if (nums[pos] - nums[pos - 1] >= 1)
+            {
+                len++;
+                start = pos - len;
+
+                if (len > bestLenght)
+                {
+                    bestLenght = len;
+                    bestStart = start;
+                }
+            }
+            else
+            {
+                len = 0;
+            }
         }
+
+        for (int i = bestStart; i <= (bestStart + bestLenght); i++)
+        {
+            Console.Write($"{nums[i]} ");
+        }
+
+        Console.WriteLine();
     }
 }
+
