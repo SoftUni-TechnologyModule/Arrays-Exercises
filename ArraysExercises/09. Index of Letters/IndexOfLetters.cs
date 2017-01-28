2384e684c8cb7ef89440a14829810271a1fefd70
VenciSarov <venci.sarov@gmail.com>
2017-01-28 15:08:24 +0200

    Problem solved.

--- ArraysExercises/09. Index of Letters/IndexOfLetters.cs
+++ ArraysExercises/09. Index of Letters/IndexOfLetters.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _09.Index_of_Letters
+class IndexOfLetters
 {
-    class IndexOfLetters
+    static void Main()
     {
-        static void Main(string[] args)
+        char[] word = Console.ReadLine().ToLower().ToArray();
+
+        GetLettersIndex(word);
+    }
+
+    static void GetLettersIndex(char[] word)
+    {
+        char[] alphabet = new char[26];
+        char firstLetter = 'a';
+
+        for (int i = 0; i < alphabet.Length; i++, firstLetter++)
         {
+            alphabet[i] = firstLetter;
+        }
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int r = 0; r < alphabet.Length; r++)
+            {
+                if (word[i] == alphabet[r])
+                {
+                    Console.WriteLine($"{word[i]} -> {r}");
+                    break;
+                }
+            }
         }
     }
 }
+
